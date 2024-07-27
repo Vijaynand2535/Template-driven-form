725355d26b4ad2b09dc54c60653440cee5c72f31
Benkunde Vijaynand Dhanraj <140649943+Vijaynand2535@users.noreply.github.com>
2024-07-27 23:13:03 +0530

    work on all task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Tdform';
+  @ViewChild("signup") signup!: NgForm
+  mocArr = [
+  {contactMode : 'Email', id : 123},
+  {contactMode : 'Phone', id : 124}
+  ]
+  userdetails = {
+  "userInfo": { 
+    "username": "vijay benkunde", 
+    "email": "vijay@gmail.com" 
+  }, 
+    "secretquestions": "favouritebook", 
+    "answer": "my book", 
+    "isSubscribe": true, 
+    "modeofcontact": "Email" 
+
+  }
+  constructor(){
+
+  }
+ngOnInit(): void {
+  
+}
+  onSignUp(signup : NgForm){
+    
+    // if(signup.valid){
+    //   // console.log(signup.valid);
+    //   // console.log(signup);
+    //   console.log(signup.value);
+      
+    // }
+    console.log(signup.value);
+  }
+  onEdit(){
+    this.signup.form.patchValue(this.userdetails)
+  }
 }
